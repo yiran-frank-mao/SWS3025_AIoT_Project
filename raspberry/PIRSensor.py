27a50f1db84db62166ecc6a29a99d837ece5cdbe
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-08 10:17:43 +0800

    done pir

--- raspberry/PIRSensor.py
+++ raspberry/PIRSensor.py
@@ -1,9 +1,20 @@
 from Sensors import Sensor
+import RPi.GPIO as GPIO
 
 class PIRSensor(Sensor):
-    def __init__(self, name, motion):
+    def __init__(self, name, pirSensorPin):
         super().__init__(name)
+        self.pin = pirSensorPin
 
-    def get_value(self):
-        #TODO: Implement this method
-        raise NotImplementedError
+    def get_value(self) -> bool:
+        # Returns true if motion is detected, false otherwise
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN)
+        rtn = GPIO.input(self.pin)
+        GPIO.cleanup()
+        return rtn
+
+def PIRTest():
+    pir = PIRSensor("PIR", 12)
+    while True:
+        print(pir.get_value())

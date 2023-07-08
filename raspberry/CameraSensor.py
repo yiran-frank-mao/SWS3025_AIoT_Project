f4bcc5fc0f6e418f2936e55a637dc0c8b0b3cc64
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-08 11:58:06 +0800

    update camera sensor

--- raspberry/CameraSensor.py
+++ raspberry/CameraSensor.py
@@ -1,9 +1,19 @@
 from Sensors import Sensor
+from picamera import PiCamera
+from datetime import datetime
 
 class CameraSensor(Sensor):
     def __init__(self, name):
         super().__init__(name)
 
-    def get_value(self):
+    def get_value(self) -> str:
         #TODO: Implement this method
-        raise NotImplementedError
+        camera = PiCamera()
+        filePath = '/home/pi/images'
+        fileName = datetime.now().strftime("%Y%m%d%H%M%S") + '.jpg'
+        camera.capture(filePath + '/' + fileName)
+        return filePath + '/' + fileName
+
+def camera_sensor_test():
+    camera = CameraSensor("Camera")
+    print(camera.get_value())

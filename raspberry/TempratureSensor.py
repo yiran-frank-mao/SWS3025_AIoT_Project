74bd9f5e2b1dbf551cf4923424ec6224019633a5
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-08 12:55:27 +0800

    Update TempratureSensor.py

--- raspberry/TempratureSensor.py
+++ raspberry/TempratureSensor.py
@@ -1,9 +1,23 @@
 from Sensors import Sensor
+import Adafruit_DHT
 
 class TemperatureSensor(Sensor):
-    def __init__(self, name):
+    def __init__(self, name, GPIO_Pin=17):
         super().__init__(name)
+        self.sensor_type = Adafruit_DHT.DHT11
+        self.pin = GPIO_Pin
 
-    def get_value(self):
+    def get_value(self) -> (float, float):
         #TODO: Implement this method
-        raise NotImplementedError
+        humidity, temperature = Adafruit_DHT.read_retry(self.sensor_type, self.pin)
+        return temperature, humidity
+
+def TemperatureSensor_test():
+    sensor = TemperatureSensor("DHT11")
+    temperature, humidity = sensor.get_value()
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+TemperatureSensor_test()

a7346054d87d57649f7d3aca2e0eb1ca6e2e19a2
tuninguu <136546166+tuninguu@users.noreply.github.com>
2023-07-10 18:42:17 +0800

    Update LightSensor.py

--- raspberry/LightSensor.py
+++ raspberry/LightSensor.py
@@ -6,20 +6,14 @@ light = MCP3008(0)
 led = PWMLED(21) # GPIO 21 or Pin 40
 led.on() # test - the led should be lit
 time.sleep(1)
-led.off() # test - the led should go off
-time.sleep(1)
-led.value = 0.5 # test - the led should be lit at half brightness
-time.sleep(1)
-led.off()
-time.sleep(1)
 
 led.source = light.values # actual connecting of the LED to the potentiometer
-print("the light value = %d" %light)
 print("Program running... Press CTRL+C to exit")
 
 try:
 
 	while True:
+		print("the light value = ", light.value)
 		time.sleep(0.1)
 
 except KeyboardInterrupt:

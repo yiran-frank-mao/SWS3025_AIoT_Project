c9f902ff1f217daba52ddd288d9c4a1aba3d4aef
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-12 19:13:32 +0800

    Update testing.py

--- web_API/testing.py
+++ web_API/testing.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request, session, redirect, url_for
 from flask import send_from_directory
 from flask import render_template
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,11 +12,13 @@ def index():
 
 @app.route('/api/sensors/temperature')
 def get_temperature():
-    return 26.5
+    y = 26
+    return str(y+random.randint(-5, 5))
 
 @app.route('/api/sensors/humidity')
 def get_humidity():
-    return 60
+    x = 60
+    return str(x+random.randint(-5, 5))
 
 if __name__ == '__main__':
     app.run(port=8080)

6fd1451e7fe4f6090b10506b0e76788c815d25ac
ShirOYOri <baiyanchi1220@163.com>
2023-07-12 19:07:31 +0800

    光传感器

--- Sensors/LightSensor.py
+++ Sensors/LightSensor.py
@@ -20,9 +20,46 @@ from gpiozero import PWMLED
 # except KeyboardInterrupt:
 # 	print("Program terminated!")
 
+
+#调光函数：while 1 if光强>(上阈值)→led变亮 else if光强<(下阈值)→led变暗
+#打开灯
 class lightSensor(Sensor):
-	def __init__(self, name):
-		self.name = name
+	def __init__(self):
+		led = PWMLED(21)
+		light = MCP3008(0)
+	def ledstate(module): #每进行一次相机采样模式识别就执行一次
+		if module = 'manual': #手动模式
+			led.value = 0.8 #默认亮度
+			led.on()
+		elif module = 'reading': #阅读模式
+		    led.value = 0.8  # 默认亮度
+		    led.on()
+            ideallight = 0.009
+	        i = 0
+	        while(i<100):
+				adjust(ideallight)
+		        i = i + 1
+		elif module = 'computer':#电脑模式
+	        led.value = 0.8  # 默认亮度
+	        led.on()
+			ideallight = 0.02
+	        i = 0
+	        while(i<100):
+				adjust(ideallight)
+		        i = i + 1
+            #此处有一个循环执行adjust（ideallight）直到灯关闭/切换为手动模式但我不会写
+	def ledoff():
+            led.off()
+	def adjust(ideallight):
+		light0 = get_value()
+                if light0 > ideallight + 0.001#暗
+			        if led.value + 0.001 < 1 :
+					    led.value = led.value + 0.001
+					else led.value = 1
+			    elif light0 < ideallight - 0.001#亮
+					if led.value - 0.001 > 0 :
+		                led.value = led.value - 0.001
+			        else led.value = 0
 
 	def get_value(self):
-		pass
+		self = light.value

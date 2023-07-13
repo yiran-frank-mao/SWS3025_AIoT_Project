cfcbe6809c1971fd06357ba484eaece4c6d299b8
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-13 15:43:14 +0800

    update web

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from Sensors.TemperatureSensor import TemperatureSensor
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='web')
-cors = CORS(app)
+cors_app = CORS(app)
+api = Flask(__name__)
+cors_api = CORS(api)
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/web/<path:name>')
 def return_flutter_doc(name):
@@ -21,20 +24,16 @@ def return_flutter_doc(name):
     return send_from_directory(DIR_NAME, datalist[-1])
 
 
-@app.route('/welcome')
-def welcome():
-    return 'Welcome to smart hub'
-
-
-@app.route('/api/sensors/temperature')
+@api.route('/api/sensors/temperature')
 def get_temperature():
     return str(TemperatureSensor().get_value()[0])
 
 
-@app.route('/api/sensors/humidity')
+@api.route('/api/sensors/humidity')
 def get_humidity():
     return str(TemperatureSensor().get_value()[1])
 
 
 if __name__ == '__main__':
+    api.run(port=8123)
     app.run(host='0.0.0.0', port=8080)

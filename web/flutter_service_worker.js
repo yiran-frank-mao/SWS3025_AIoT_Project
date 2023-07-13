81111974faf820ad8c24716688cda9fe486fea4c
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-13 16:00:49 +0800

    update webpage

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c3baed92020ab261923cd49f6cbfee92",
-"index.html": "2b7daed415e1d8aed0d0d4b9ea09a750",
-"/": "2b7daed415e1d8aed0d0d4b9ea09a750",
-"main.dart.js": "58192ee60cb4e1159d913b8c8a33e47c",
+"index.html": "2e9245777fabc043e1e217b89d95f708",
+"/": "2e9245777fabc043e1e217b89d95f708",
+"main.dart.js": "4381a78a3d3df6afa8bcd27c326c6054",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",

54be28b0c9759a3b8d9addcdb4287340fad3c871
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-14 14:10:58 +0800

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,7 +1,26 @@
-from picamera import PiCamera
-from time import sleep
+import cv2
 
-camera = PiCamera()
-camera.start_preview()
-while True:
-    sleep(1)
+count = 0  # 定义拍照计数变量
+
+
+def main():
+    global count
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)  # 设置分辨率
+    cap.set(4, 480)
+    while cap.isOpened():
+        ret, img = cap.read()  # 视频读入
+        if not ret:
+            continue
+        cv2.imshow('show', img)
+        key = cv2.waitKey(30) & 0xFF
+        if key == ord("q"):
+            break
+        elif key == ord("s"):
+            count = count + 1
+            cv2.imwrite("./testback" + str(count) + ".jpg", img)
+            print("save success!  count =", count)
+
+
+if __name__ == '__main__':
+    main()

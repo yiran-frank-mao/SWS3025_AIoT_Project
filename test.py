72238d6d59ce944d27d99387eb8903d54d081ae4
ShirOYOri <baiyanchi1220@163.com>
2023-07-15 15:14:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,11 +11,10 @@ def lightControllerTest():
     pir = PIRSensor()
     lightSensor = LightSensor()
     lightController = LightController(lightSensor, pir)
-    lightController.dark()
     lightController.led_on()
     lightController.set_led(1)
     # lightController.adjustTo(0.8)
-    # print(lightController.lightSensor.get_value())
+    print(lightController.lightSensor.get_value())
     input("Press Enter to continue...")
 
 def testMicrobit():
@@ -30,15 +29,16 @@ def PIRTest():
     pir = PIRSensor()
     lightSensor = LightSensor()
     lightController = LightController(lightSensor, pir)
-    # while True:
-    #     time.sleep(0.1)
-    #     print(pir.get_value())
-    #     if pir.get_value() == 1:
-    #         print("led",lightController.get_led())
-    #         lightController.led_on()
-    #         print("led",lightController.get_led())
-    #         time.sleep(30)
-    #         light.led_off()
+    while True:
+        time.sleep(0.1)
+        print(pir.get_value())
+        if self.PIRSensor.get_value() == 1:
+            print("led", lightController.get_led())
+            lightController.led_on()
+            lightController.set_led(0.8)
+            print("led", lightController.get_led())
+            time.sleep(30)
+            lightController.led_off()
     lightController.led_on()
 
 

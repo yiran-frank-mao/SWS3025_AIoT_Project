4b4d1d1127c5d046ce7c370584d9a3ced441edf2
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-17 12:30:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import render_template, send_from_directory
 from api import app
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/web/<path:name>')
 def return_flutter_doc(name):

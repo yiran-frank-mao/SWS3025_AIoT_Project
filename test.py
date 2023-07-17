59c5e0e0726d1c89b5411036c6898f25b51db1e3
tuninguu <136546166+tuninguu@users.noreply.github.com>
2023-07-17 18:57:55 +0800

    lc3

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
-import requests
+#import requests
 import time
 from LightController import LightController
-from Sensors.CameraSensor import ImageSensor, VideoSensor
-from Sensors.LightSensor import LightSensor
-from Sensors.PIRSensor import PIRSensor
-from Sensors.TemperatureSensor import TemperatureSensor
-from MicrobitCommunication import MicCom
+#from Sensors.CameraSensor import ImageSensor, VideoSensor
+#from Sensors.LightSensor import LightSensor
+#from Sensors.PIRSensor import PIRSensor
+#from Sensors.TemperatureSensor import TemperatureSensor
+#from MicrobitCommunication import MicCom
 
 
 
 if __name__ == '__main__':
     mode = 'reading'
     lightController = LightController()
+    i = 0
 
-    while True:
+    for i in range(10):
         lightController.set_state(mode)
         time.sleep(2)

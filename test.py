0af61dc55c653c8d37029a82d37e74203c5a1067
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-19 14:31:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,35 @@ import time
 from pathlib import Path
 from tqdm import tqdm
 
-# from Alarm import Alarm
-# from Controller import Controller
-# from Sensors.CameraSensor import ImageSensor, VideoSensor
-# from Sensors.LightSensor import LightSensor
-# from Sensors.PIRSensor import PIRSensor
-# from Sensors.TemperatureSensor import TemperatureSensor
-# from MicrobitCommunication import MicCom
-# from Buzz import Buzz
+from Alarm import Alarm
+from Controller import Controller
+from ModeDetector import ModeDetector
+from Sensors.CameraSensor import ImageSensor, VideoSensor
+from Sensors.LightSensor import LightSensor
+from Sensors.PIRSensor import PIRSensor
+from Sensors.TemperatureSensor import TemperatureSensor
+from MicrobitCommunication import MicCom
+from Buzz import Buzz
 
 if __name__ == '__main__':
-    for item in tqdm(Path("ml/images").glob('*')):
-        if item.is_file():
-            item.unlink()
+    image_sensor = ImageSensor("Image")
+    video_sensor = VideoSensor("Video")
+    temperature_sensor = TemperatureSensor()
+    pir = PIRSensor()
+    buzz = Buzz()
+    alarm = Alarm()
+    microbit = MicCom()
+    lightSensor = LightSensor()
+    modeDetector = ModeDetector()
+
+    controller = Controller(
+        lightSensor=lightSensor,
+        pirSensor=pir,
+        imageSensor=image_sensor,
+        modeDetector=modeDetector,
+        alarm=alarm,
+        buzzer=buzz,
+        microbit=microbit)
+
+    controller.led_off()
+    controller.adjustTo(0.8)

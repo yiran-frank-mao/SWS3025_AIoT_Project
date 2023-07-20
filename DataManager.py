1e73394d9bc344296949bca23a01d555a507d173
Yiran <89192652+RavenMFrank@users.noreply.github.com>
2023-07-20 20:15:25 +0800

    Update DataManager.py

--- DataManager.py
+++ DataManager.py
@@ -1,10 +1,13 @@
 import sqlite3
 import threading
 import time
+
+import Controller
 import Sensors.TemperatureSensor
 
 
 def initialize():
+    print("Initializing database...", end="")
     con = sqlite3.connect("database.db")
     cur = con.cursor()
     cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
@@ -16,20 +19,23 @@ def initialize():
         cur.execute(f"DROP TABLE {table_name}")
     cur.execute("CREATE TABLE IF NOT EXISTS temperature (id INTEGER PRIMARY KEY, time TEXT, value REAL)")
     cur.execute("CREATE TABLE IF NOT EXISTS humidity (id INTEGER PRIMARY KEY, time TEXT, value REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS mode (id INTEGER PRIMARY KEY, time TEXT, mode TEXT)")
     con.commit()
     cur.close()
     con.close()
+    print("  --  Database initialized.")
 
 
-def dataManage(tempSensor: Sensors.TemperatureSensor.TemperatureSensor):
+def dataManage(tempSensor: Sensors.TemperatureSensor.TemperatureSensor, controller: Controller):
     con = sqlite3.connect("database.db")
     cur = con.cursor()
     temp = tempSensor.get_value()[0]
     humidity = tempSensor.get_value()[1]
     now = time.strftime("%Y-%m-%d %H:%M:%S")
-    cur.execute("INSERT INTO temperature (time, value) VALUES (?, ?)", (now, temp))
-    cur.execute("INSERT INTO humidity (time, value) VALUES (?, ?)", (now, humidity))
+    # cur.execute("INSERT INTO temperature (time, value) VALUES (?, ?)", (now, temp))
+    # cur.execute("INSERT INTO humidity (time, value) VALUES (?, ?)", (now, humidity))
+    cur.execute("INSERT INTO mode (time, mode) VALUES (?, ?)", (now, controller.mode))
     # 提交更改
     con.commit()
-    dataManageThread = threading.Timer(240, dataManage, (tempSensor,))
+    dataManageThread = threading.Timer(120, dataManage, (tempSensor,))
     dataManageThread.start()
